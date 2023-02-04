fe91b6128b1ab3547ca5d6a7dd49a4a6d4e94b3b
Mona <mona.orschmann@gmail.com>
2023-02-04 17:55:43 +0100

    fe

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,21 +1,22 @@
-import {Component, OnInit} from '@angular/core';
-import {ServerService} from "./service/server.service";
-import {AppState} from "./interface/app-state";
-import {BehaviorSubject, map, Observable, of, startWith} from "rxjs";
-import {CustomResponse} from "./interface/custom-response";
-import {DataState} from "./enum/data-state.enum";
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { catchError, map, startWith } from 'rxjs/operators';
+import { DataState } from './enum/data-state.enum';
 import { Status } from './enum/status.enum';
-import {catchError} from "rxjs/operators";
-import {Server} from "./interface/server";
-import {NgForm} from "@angular/forms";
+import { AppState } from './interface/app-state';
+import { CustomResponse } from './interface/custom-response';
+import { Server } from './interface/server';
+import { NotificationService } from './service/notification.service';
+import { ServerService } from './service/server.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
-
   appState$: Observable<AppState<CustomResponse>>;
   readonly DataState = DataState;
   readonly Status = Status;
@@ -25,18 +26,21 @@ export class AppComponent implements OnInit {
   private isLoading = new BehaviorSubject<boolean>(false);
   isLoading$ = this.isLoading.asObservable();
 
-  constructor(private serverService: ServerService) {
-  }
+
+  constructor(private serverService: ServerService, private notifier: NotificationService) { }
 
   ngOnInit(): void {
     this.appState$ = this.serverService.servers$
       .pipe(
         map(response => {
-          return {dataState: DataState.LOADED_STATE, appData: response}
+          this.notifier.onDefault(response.message);
+          this.dataSubject.next(response);
+          return { dataState: DataState.LOADED_STATE, appData: { ...response, data: { servers: response.data.servers.reverse() } } }
         }),
-        startWith({dataState: DataState.LOADING_STATE}),
+        startWith({ dataState: DataState.LOADING_STATE }),
         catchError((error: string) => {
-          return of({dataState: DataState.ERROR_STATE, error: error})
+          this.notifier.onError(error);
+          return of({ dataState: DataState.ERROR_STATE, error });
         })
       );
   }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {FormsModule} from "@angular/forms";
+import {NotifierModule} from "angular-notifier";
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import {FormsModule} from "@angular/forms";
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    NotifierModule
   ],
   providers: [],
   bootstrap: [AppComponent]
